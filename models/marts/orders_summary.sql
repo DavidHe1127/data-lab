bee7777aa4ab8dbbb692da84fe4bd88cb76a7f4d
David He <david.he@rest.com.au>
2025-12-18 19:14:44 +1100

    changes

--- models/marts/orders_summary.sql
+++ models/marts/orders_summary.sql
@@ -7,9 +7,10 @@
 select
     c.customer_id,
     c.first_name,
-    c.last_name,
+    o.status,
     count(o.order_id) as total_orders
 from {{ ref('stg_customers') }} as c
 left join {{ ref('stg_orders') }} as o
     on c.customer_id = o.customer_id
-group by c.customer_id, c.first_name, c.last_name
+where o.status is not null
+group by c.customer_id, c.first_name, o.status
